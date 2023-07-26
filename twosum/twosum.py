6286a4e80609d311bb9680d4dd86e6bddf29d6a9
tim <timotif@gmail.com>
2023-07-26 15:26:58 +0200

    comment

--- twosum/twosum.py
+++ twosum/twosum.py
@@ -1,4 +1,7 @@
 class Solution:
+    # Setting up a basic hash table to store the difference between the target and the current number
+    # If the current number is in the hash table, then we have found the pair
+    # If not, then we add the difference to the hash table
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         num_hash = {}
         for idx, num in enumerate(nums):
